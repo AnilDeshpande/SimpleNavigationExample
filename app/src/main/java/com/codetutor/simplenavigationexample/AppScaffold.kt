cb45124de9e674f2a25a5d3b702f7ae7610e0ee1
Anil Deshpande <anildesh82@gmail.com>
2024-02-08 20:03:34 +0530

    Navigation icon made dynamic with click actions added

--- app/src/main/java/com/codetutor/simplenavigationexample/AppScaffold.kt
+++ app/src/main/java/com/codetutor/simplenavigationexample/AppScaffold.kt
@@ -1,8 +1,10 @@
 package com.codetutor.simplenavigationexample
 
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -14,6 +16,9 @@ import androidx.compose.material3.TopAppBar
 
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -27,9 +32,12 @@ import com.codetutor.simplenavigationexample.screens.ScreenTwo
 @Composable
 fun AppScaffold(navController: NavHostController){
 
+    var isBackEnabled = remember { mutableStateOf(false) }
+
     navController.addOnDestinationChangedListener { controller, destination, arguments ->
         // Handle destination change
         Log.d("NavController", "Destination changed to ${destination.route}")
+        isBackEnabled.value = destination.route == "screen-one"
     }
 
     Scaffold(
@@ -44,8 +52,17 @@ fun AppScaffold(navController: NavHostController){
                     Text(text = "CountryInfoApp", style = MaterialTheme.typography.labelLarge)
                 },
                navigationIcon = {
+                   var navIcon = if (isBackEnabled.value){
+                       Icons.Filled.Home
+                   }else{
+                       Icons.Filled.ArrowBack
+                   }
                     IconButton(onClick = { /*TODO*/ }) {
-                        Icon(imageVector = Icons.Filled.Home, contentDescription ="ArrowBack")
+                        Icon(imageVector = navIcon, contentDescription ="ArrowBack", Modifier.clickable {
+                            if(!isBackEnabled.value){
+                                navController.popBackStack()
+                            }
+                        })
                     }
                 }
 
