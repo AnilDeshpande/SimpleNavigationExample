ff86578b4fbb9ba52aa0fe18608f1d470a44903a
Anil Deshpande <anildesh82@gmail.com>
2024-02-10 16:52:12 +0530

    navigation fragment settup up code.
    Non Working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,6 @@ dependencies {
 
 
     implementation("androidx.navigation:navigation-compose:$nav_version")
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
