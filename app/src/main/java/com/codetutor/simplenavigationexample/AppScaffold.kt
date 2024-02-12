9e3c9d352fada9cdd2926c6a96cc3e8188f33b18
Anil Deshpande <anildesh82@gmail.com>
2024-02-12 21:40:44 +0530

    initial working commit

--- app/src/main/java/com/codetutor/simplenavigationexample/AppScaffold.kt
+++ app/src/main/java/com/codetutor/simplenavigationexample/AppScaffold.kt
@@ -17,14 +17,16 @@ import androidx.compose.material3.TopAppBar
 
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.createGraph
 import com.codetutor.simplenavigationexample.screens.ScreenOne
 import com.codetutor.simplenavigationexample.screens.ScreenThree
 import com.codetutor.simplenavigationexample.screens.ScreenTwo
@@ -71,23 +73,22 @@ fun AppScaffold(navController: NavHostController){
             )
         },
     ) { paddingValues ->
-
-        NavHost(navController, startDestination = "screen-one"){
-            navigationGraph(navController, paddingValues)
-        }
+        NavHost(navController = navController, graph = getMyAppNavGraph(navController, paddingValues))
     }
 }
 
-fun NavGraphBuilder.navigationGraph(navController: NavHostController, paddingValues: PaddingValues){
-    composable("screen-one"){
-        ScreenOne(navController, modifier = Modifier.padding(paddingValues))
-    }
+fun getMyAppNavGraph(navController: NavController, paddingValues: PaddingValues): NavGraph {
+    return navController.createGraph(startDestination = "screen-one"){
+        composable("screen-one"){
+            ScreenOne(navController, modifier = Modifier.padding(paddingValues))
+        }
 
-    composable("screen-two"){
-        ScreenTwo(navController, modifier = Modifier.padding(paddingValues))
-    }
+        composable("screen-two"){
+            ScreenTwo(navController, modifier = Modifier.padding(paddingValues))
+        }
 
-    composable("screen-three"){
-        ScreenThree(navController, modifier = Modifier.padding(paddingValues))
+        composable("screen-three"){
+            ScreenThree(navController, modifier = Modifier.padding(paddingValues))
+        }
     }
 }
