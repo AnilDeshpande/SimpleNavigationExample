4b2e3a8a471a3fa13f163e2782db907d03a8f989
Anil Deshpande <anildesh82@gmail.com>
2024-02-19 16:57:48 +0530

    Video 32 - Dissecting Nav Controller

--- app/src/main/java/com/codetutor/simplenavigationexample/App.kt
+++ app/src/main/java/com/codetutor/simplenavigationexample/App.kt
@@ -1,11 +1,16 @@
 package com.codetutor.simplenavigationexample
 
+import android.os.Bundle
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.createGraph
 import com.codetutor.simplenavigationexample.components.MyAppScaffold
 import com.codetutor.simplenavigationexample.screens.ScreenOne
 import com.codetutor.simplenavigationexample.screens.ScreenThree
@@ -17,14 +22,22 @@ fun App(){
     //NavController
     val navController = rememberNavController()
 
-    //NavHost
-    NavHost(navController = navController, startDestination = "screen-one"){
-        navigationGraph(navController = navController)
+    navController.addOnDestinationChangedListener { controller,
+                                                    destination,
+                                                    arguments ->
+
+        Log.i("NavController","Destination: ${destination.route}")
+        Log.i("NavController","Controller: ${controller.currentDestination?.route}")
+
     }
+
+    //NavHost
+//    NavHost(navController = navController, startDestination = "screen-one"){
+//        navigationGraph(navController = navController)
+//    }
+    
+    NavHost(navController = navController, graph = getMyNavGraph(navController))
 }
-
-
-
 
 
 //NavGraph
@@ -40,5 +53,21 @@ fun NavGraphBuilder.navigationGraph(navController: NavController){
 
     composable("screen-three"){
         ScreenThree(navController)
+    }
+}
+
+fun getMyNavGraph(controller: NavController): NavGraph {
+    return controller.createGraph(startDestination = "screen-one"){
+        composable("screen-one"){
+            ScreenOne(controller)
+        }
+
+        composable("screen-two"){
+            ScreenTwo(controller)
+        }
+
+        composable("screen-three"){
+            ScreenThree(controller)
+        }
     }
 }
