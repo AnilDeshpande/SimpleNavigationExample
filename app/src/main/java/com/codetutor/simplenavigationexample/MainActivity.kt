e0b46cc8b6dc3a381ee96e77f856d7d35b08384d
Anil Deshpande <anildesh82@gmail.com>
2024-03-06 20:43:09 +0530

    removed unused imports

--- app/src/main/java/com/codetutor/simplenavigationexample/MainActivity.kt
+++ app/src/main/java/com/codetutor/simplenavigationexample/MainActivity.kt
@@ -6,13 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
-import com.codetutor.simplenavigationexample.components.screens.ScreenOne
-import com.codetutor.simplenavigationexample.components.screens.ScreenThree
-import com.codetutor.simplenavigationexample.components.screens.ScreenTwo
 import com.codetutor.simplenavigationexample.ui.theme.SimpleNavigationExampleTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/codetutor/simplenavigationexample/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/codetutor/simplenavigationexample/viewmodels/SharedViewModel.kt
@@ -2,7 +2,6 @@ package com.codetutor.simplenavigationexample.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.codetutor.simplenavigationexample.data.AppRepository
 import com.codetutor.simplenavigationexample.data.Student
 
 class SharedViewModel : ViewModel() {
