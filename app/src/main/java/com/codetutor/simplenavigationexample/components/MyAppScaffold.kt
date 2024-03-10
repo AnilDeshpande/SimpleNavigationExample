f7f83f06db4df11934b5a763e16ce725bfb992bf
Anil Deshpande <anildesh82@gmail.com>
2024-03-10 12:09:53 +0530

    minor refactoring

--- app/src/main/java/com/codetutor/simplenavigationexample/components/MyAppScaffold.kt
+++ app/src/main/java/com/codetutor/simplenavigationexample/components/MyAppScaffold.kt
@@ -1,8 +1,6 @@
 package com.codetutor.simplenavigationexample.components
 
 import android.util.Log
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
@@ -17,20 +15,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import com.codetutor.simplenavigationexample.utils.getMyNavGraph
-import com.codetutor.simplenavigationexample.screens.ScreenOne
-import com.codetutor.simplenavigationexample.screens.ScreenThree
-import com.codetutor.simplenavigationexample.screens.ScreenTwo
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyAppScaffold ( startDestination: String, navController: NavHostController) {
+fun MyAppScaffold(startDestination: String, navController: NavHostController) {
 
     var isBackEnabled = remember {
         mutableStateOf(false)
@@ -40,13 +31,13 @@ fun MyAppScaffold ( startDestination: String, navController: NavHostController) 
                                                     destination,
                                                     arguments ->
 
-        Log.i("NavController","Destination: ${destination.route}")
-        isBackEnabled.value = destination.route =="screen-one"
+        Log.i("NavController", "Destination: ${destination.route}")
+        isBackEnabled.value = destination.route == "screen-one"
     }
 
     Scaffold(
         topBar = {
-            TopAppBar (
+            TopAppBar(
                 colors = TopAppBarDefaults.smallTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
@@ -58,23 +49,30 @@ fun MyAppScaffold ( startDestination: String, navController: NavHostController) 
 
                 navigationIcon = {
 
-                    var navIcon = if(isBackEnabled.value){
+                    var navIcon = if (isBackEnabled.value) {
                         Icons.Filled.Home
-                    }else{
+                    } else {
                         Icons.Filled.ArrowBack
                     }
 
-                    IconButton(onClick = { if(!isBackEnabled.value) { navController.popBackStack() } }) {
+                    IconButton(onClick = {
+                        if (!isBackEnabled.value) {
+                            navController.popBackStack()
+                        }
+                    }) {
                         Icon(imageVector = navIcon, contentDescription = "NavIcon")
                     }
                 }
-                
+
             )
         },
 
-    ) { paddingValues ->
+        ) { paddingValues ->
         //Add the app scaffold here and pass the padding values to scaffold
-        NavHost(navController = navController, graph = getMyNavGraph(startDestination, navController, paddingValues))
+        NavHost(
+            navController = navController,
+            graph = getMyNavGraph(startDestination, navController, paddingValues)
+        )
     }
 }
 
